f95cc09d7c601c91246b002551fad0a36acb998b
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:03:12 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan7/PBO110118033Latihan7.java
@@ -0,0 +1,27 @@
+package pbo1.pkg10118033.latihan7;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan7 {
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " +b);
+        System.out.println("short : " +s);
+        System.out.println("int   : " +i);
+        System.out.println("long  : " +l);
+    }
+}
